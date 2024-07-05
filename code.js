47e906274011a5afd110c4d659d6332360f15dd4
fabian412 <fabian.orma221@gmail.com>
2024-07-05 17:46:10 -0400

    Actualizacion archivo js
    Termino del proyecto, con las funciones en java. Aun falta modelo responsivo

---
+++ code.js
@@ -0,0 +1,54 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = textoCifrado;
+      parrafo.textContent = "";
+      document.getElementById("muñeco").style.display = "none";
+      document.getElementById("copiar").style.display = "block";
+
+      
+    } else {
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Porfavor, ingrese un mensaje");
+    }
+  }
+ function copiar() {
+  var textoCopiado = document.getElementById("texto");
+  textoCopiado.select();
+  textoCopiado.setSelectionRange(0, 99999); 
+  navigator.clipboard.writeText(textoCopiado.value);
+ }
+
+ function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo"); 
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = textoCifrado;
+        parrafo.textContent = "";
+      } else {
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      }
+  }
+  
